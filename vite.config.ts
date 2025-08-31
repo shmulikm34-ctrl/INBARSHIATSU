c1f6b5f805421dcd529781d6be97946e2bd02baf
shmulikm34 <shmulikm34@users.noreply.github.com>
2025-08-31 15:07:01 +0300

    fix(build): move vite & plugin to dependencies and set dynamic base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Vercel מגדירה VERCEL=1 בסביבת הבילד
+const isVercel = process.env.VERCEL === "1";
+const isGh = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
-  base: "/INBARSHIATSU/",
+  base: isGh ? "/INBARSHIATSU/" : "/", // GH Pages לעומת Vercel/כל מקום אחר
   plugins: [react()],
 });
