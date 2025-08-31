97c25273bbac0028ac7c602ee7794a09bf3fd095
shmulikm34 <shmulikm34@users.noreply.github.com>
2025-08-31 15:00:29 +0300

    fix(build): add Vite React plugin & build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isGh = process.env.GITHUB_PAGES === "true";
-
 export default defineConfig({
-  base: isGh ? "/inbar-shiatsu/" : "/",
+  base: "/INBARSHIATSU/",
   plugins: [react()],
 });
